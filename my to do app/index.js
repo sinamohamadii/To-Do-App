18a29a7bbf11f1f3e7e034dd3078ee0cb2c13494
sina mohamadi <sina.m925@gmail.com>
2022-07-25 18:41:25 +0430

    Completed whole script

--- my to do app/index.js
+++ my to do app/index.js
@@ -37,7 +37,7 @@ function addNewNote() {
     }
     else {
         notesContainer.innerHTML +=
-            `<div class="px-2 w-full xs:w-1/2 md:!w-1/3 lg:!w-1/4 self-start">
+            `<div class="note-${index} px-2 w-full xs:w-1/2 md:!w-1/3 lg:!w-1/4 self-start">
                 <div class="w-full h-auto rounded-xl bg-white py-3 flex flex-col gap-y-5 overflow-hidden">
                     <div class="w-full border-l-[5px] border-gray-500 h-auto pl-5 flex flex-col">
                         <span class="font-semibold">${titleInput.value}</span>
@@ -66,7 +66,7 @@ function addNewNote() {
                         </div>
                         <button 
                             data-importance="false"
-                            class="text-red-500 px-3 py-2 rounded-md hover:text-red-300" 
+                            class="note-btn-${index} text-red-500 px-3 py-2 rounded-md hover:text-red-300" 
                             onclick="importance(this, ${index})">
                             Important
                         </button>
@@ -78,75 +78,54 @@ function addNewNote() {
         newNoteModal.classList.add("hidden");
         newNoteModalToggle = "close";
     }
+    replaceClass(notesContainer, "hidden", "flex");
+    tab.classList.add("active-tab");
+    replaceClass(importantNotesContainer, "flex", "hidden");
+    importantTab.classList.remove("active-tab");
 
 }
 
 /* use to delete a note */
 function removeNote(n) {
 
-    for (var i = 0; i < indexArray.length; i++) {
+    let note = document.querySelectorAll(`.note-${n}`);
 
-        if (indexArray[i] === n) {
-            notesContainer.removeChild(notesContainer.children[i]);
-            indexArray.splice(i, 1);
-        }
-    }
-
-    if (n === 1) {
-        index = 0;
+    let i;
+    for (i = 0; i < note.length; i++) {
+        note[i].remove();
     }
 
 }
 
-/* use to star a note */
-const star = `<svg width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-</svg>`;
-const starfill = `<svg width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-</svg>`;
-
-function starNote(el) {
-
-    if (el.dataset.active === "false") {
-        el.innerHTML = starfill;
-        el.dataset.active = "true";
-    }
-    else if (el.dataset.active === "true") {
-        el.innerHTML = star;
-        el.dataset.active = "false";
-    }
-
-}
 
 /* to make important note */
 function importance(el, n) {
 
+    let note = document.querySelector(`.note-${n}`);
+
     if (el.dataset.importance === "true") {
-        el.classList.remove("active-importance-btn");
-        el.dataset.importance = "false";
-        console.log("false");
+        let importNote = document.querySelectorAll(`.note-${n}`);
+        let importNoteBtn = document.querySelectorAll(`.note-btn-${n}`);
 
-        for (var i = 0; i < indexArray.length; i++) {
-            let clone = notesContainer.children[i].cloneNode(true);
+        for (let i = 0; i < importNoteBtn.length; i++) {
+            importNoteBtn[i].classList.remove("active-importance-btn");
+            importNoteBtn[i].dataset.importance = "false";
 
-            if (indexArray[i] === n) {
-                importantNotesContainer.removeChild(clone);
-            }
         }
-    } 
-    else if(el.dataset.importance === "false") {
-        el.classList.add("active-importance-btn");
-        el.dataset.importance = "true";
-        console.log("true");
 
-        for (var i = 0; i < indexArray.length; i++) {
-            let clone = notesContainer.children[i].cloneNode(true);
+        importNote[1].remove();
+    }
+    else if (el.dataset.importance === "false") {
+        let importNoteBtn = document.querySelectorAll(`.note-btn-${n}`);
 
-            if (indexArray[i] === n) {
-                importantNotesContainer.appendChild(clone);
-            }
+        for (let i = 0; i < importNoteBtn.length; i++) {
+            importNoteBtn[i].classList.add("active-importance-btn");
+            importNoteBtn[i].dataset.importance = "true";
+
         }
+
+        let clone = note.cloneNode(true);
+        importantNotesContainer.appendChild(clone);
     }
 
 }
@@ -168,6 +147,28 @@ function switchTab(n) {
         tab.classList.remove("active-tab");
         replaceClass(importantNotesContainer, "hidden", "flex");
         importantTab.classList.add("active-tab");
+    }
+
+}
+
+
+/* use to star a note */
+const star = `<svg width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+</svg>`;
+const starfill = `<svg width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+</svg>`;
+
+function starNote(el) {
+
+    if (el.dataset.active === "false") {
+        el.innerHTML = starfill;
+        el.dataset.active = "true";
+    }
+    else if (el.dataset.active === "true") {
+        el.innerHTML = star;
+        el.dataset.active = "false";
     }
 
 }
